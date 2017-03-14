bdccd11d577443279202297a2b10a515515f520a
Vladimir <neoProgrammer@bk.ru>
2017-03-14 23:07:42 +0300

    FirstCommit

---
+++ app/src/main/java/shalk_off/raspisanie/bus/ru/raspisanie_bus/MainActivity.java
@@ -0,0 +1,13 @@
+package shalk_off.raspisanie.bus.ru.raspisanie_bus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
